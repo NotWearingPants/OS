c6ea548cc276456bc1f551719a991c9bb773d97f
Snir Broshi <snir@broshi.net>
2018-12-01 19:25:08 +0200

    add 9 new unimplemented filesystem operations

--- filesystem.c
+++ filesystem.c
@@ -3,10 +3,54 @@
 
 char the_only_file[100];
 
+///////////////////////////////////////////////////////////////
+//                           FILES                           //
+///////////////////////////////////////////////////////////////
+
 char* read_file(char* filename) {
     return the_only_file;
 }
 
 void write_file(char* filename, char* contents) {
     string_copy(the_only_file, contents);
+}
+
+void /* TODO */ delete_file(/* TODO */) {
+    // TODO
+}
+
+void /* TODO */ move_file(/* TODO */) {
+    // TODO
+}
+
+void /* TODO */ is_file(/* TODO */) {
+    // TODO
+}
+
+void /* TODO */ get_file_size(/* TODO */) {
+    // TODO
+}
+
+///////////////////////////////////////////////////////////////
+//                        DIRECTORIES                        //
+///////////////////////////////////////////////////////////////
+
+void /* TODO */ make_directory(/* TODO */) {
+    // TODO
+}
+
+void /* TODO */ remove_directory(/* TODO */) {
+    // TODO
+}
+
+void /* TODO */ move_directory(/* TODO */) {
+    // TODO
+}
+
+void /* TODO */ is_directory(/* TODO */) {
+    // TODO
+}
+
+void /* TODO */ get_directory_contents(/* TODO */) {
+    // TODO
 }
--- filesystem.h
+++ filesystem.h
@@ -1,2 +1,20 @@
+/////////////////
+//    FILES    //
+/////////////////
+
 char* read_file(char* filename);
 void write_file(char* filename, char* contents);
+void /*?*/ delete_file(/* TODO */);
+void /*?*/ move_file(/* TODO */);
+void /*?*/ is_file(/* TODO */);
+void /*?*/ get_file_size(/* TODO */);
+
+/////////////////
+// DIRECTORIES //
+/////////////////
+
+void /*?*/ make_directory(/* TODO */);
+void /*?*/ remove_directory(/* TODO */);
+void /*?*/ move_directory(/* TODO */);
+void /*?*/ is_directory(/* TODO */);
+void /*?*/ get_directory_contents(/* TODO */);
