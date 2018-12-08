2cf265e3cba836e0e51b8d115ffeb619ce4f2125
Amitbc <amitbc123@gmail.com>
2018-12-08 22:51:59 +0200

    CR pointers_count

--- filesystem.c
+++ filesystem.c
@@ -3,15 +3,14 @@
 
 char memory[255];
 char* pointers[255];
-int count_files = 0;
+int pointers_count = 0;
 
 // fix the size of the memory in the future
 
 void add_data_to_memory(char* str) {
     static int pos = 0;
-    static int count = 0;
-    pointers[count] = &memory[pos];
-    count++;
+    pointers[pointers_count] = &memory[pos];
+    pointers_count++;
 
     int str_size = string_copy(&memory[pos], str);
     pos += str_size + 1; // +1 for the null terminator
@@ -22,7 +21,7 @@ void add_data_to_memory(char* str) {
 ///////////////////////////////////////////////////////////////
 
 char* read_file(char* filename) {
-    for (int i = 0; i < count_files * 2; i += 2) {
+    for (int i = 0; i < pointers_count; i += 2) {
         if (string_compare(filename, pointers[i])) {
             return pointers[i + 1];
         }
@@ -32,7 +31,6 @@ char* read_file(char* filename) {
 }
 
 void write_file(char* filename, char* contents) {
-    count_files++;
     add_data_to_memory(filename);
     add_data_to_memory(contents);
 }
