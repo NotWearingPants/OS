f67281ea7e5e001f292c46bd2dd971faf21d0677
Amitbc <amitbc123@gmail.com>
2019-01-10 23:13:03 +0200

    add arrows key

--- keyboard.h
+++ keyboard.h
@@ -2,10 +2,13 @@
 
 #define KEYBOARD_IRQ 1
 
+#define LEFT_KEY    1
+#define RIGHT_KEY   2
+#define UP_KEY      3
+#define DOUN_KEY    4
+
 // #define CAPSLK      0x3a
 
 void keyboard_handle_interrupt();
 uint8_t get_char();
-uint8_t get_special_key();
 bool key_pressed();
-bool special_key_pressed();
