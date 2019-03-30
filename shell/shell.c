46a94e4d583a199f1c5b81499e7a7b73853294c1
Amitbc <amitbc123@gmail.com>
2019-03-30 16:29:11 +0300

    add all pointer func in shell (not working)

--- shell/shell.c
+++ shell/shell.c
@@ -1,9 +1,9 @@
 #include "shell.h"
-#include "../kernel/cmos.h"
+// #include "../kernel/cmos.h"
 #include "../kernel/screen.h"
-#include "../kernel/keyboard.h"
+// #include "../kernel/keyboard.h"
 // #include "../kernel/string.h"
-#include "../kernel/filesystem.h"
+// #include "../kernel/filesystem.h"
 
 #define PROMPT ">>"
 #define PROMPT_LENGTH (sizeof(PROMPT) - 1)
@@ -12,9 +12,14 @@ uint8_t pos_y = 0;
 
 void*** arr_pointers = (void***)0x6C00;
 
+static uint8_t (*print_string)(uint8_t x, uint8_t y, char *string, uint8_t color);
+static void (*print_number)(uint8_t x, uint8_t y, uint32_t num, uint8_t color);
 static uint8_t (*string_read)(uint8_t pos_x, uint8_t pos_y, char* buffer);
+static uint8_t (*read_from_cmos)(uint8_t reg);
 static unsigned int (*string_split)(char* str, char delimeter, char** parts);
 static bool (*string_compare)(char* left, char* right);
+static bool (*read_file)(char* filename, char* buffer);
+static void (*write_file)(char* filename, char* contents);
 static bool (*string_is_empty)(char* command);
 
 void start_shell() {
@@ -23,10 +28,15 @@ void start_shell() {
 
     // typedef uint8_t (*string_read)(uint8_t x1, uint8_t x2, char* x3);
 
-    string_read = (uint8_t (*)(uint8_t pos_x, uint8_t pos_y, char* buffer))(func_arr[2]);
-    string_split = (unsigned int (*)(char* str, char delimeter, char** parts))(func_arr[5]);
-    string_compare = (bool(*)(char* left, char* right))(func_arr[6]);
-    string_is_empty = (bool (*)(char* command))(func_arr[10]);
+    print_string    = (uint8_t (*)(uint8_t x, uint8_t y, char *string, uint8_t color)) (func_arr[0]);
+    print_number    = (void (*)(uint8_t x, uint8_t y, uint32_t num, uint8_t color))    (func_arr[1]);
+    string_read     = (uint8_t (*)(uint8_t pos_x, uint8_t pos_y, char* buffer))        (func_arr[2]);
+    read_from_cmos  = (uint8_t (*)(uint8_t reg))                                       (func_arr[3]);
+    string_split    = (unsigned int (*)(char* str, char delimeter, char** parts))      (func_arr[4]);
+    string_compare  = (bool (*)(char* left, char* right))                              (func_arr[5]);
+    read_file       = (bool (*)(char* filename, char* buffer))                         (func_arr[6]);
+    write_file      = (void (*)(char* filename, char* contents))                       (func_arr[7]);
+    string_is_empty = (bool (*)(char* command))                                        (func_arr[8]);
 
     while (TRUE) {
         print_string(0, pos_y, PROMPT, DEFAULT_COLOR);
