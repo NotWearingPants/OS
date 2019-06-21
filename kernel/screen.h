1cdc54bb8b9797f74c846b34e9845f0704044f4d
Amitbc <amitbc123@gmail.com>
2019-06-21 23:53:48 +0300

    spleat test file & orgenis code

--- kernel/screen.h
+++ kernel/screen.h
@@ -1,36 +1,12 @@
-#include "../test.h"
-
 #ifndef _SCREEN_H_
 #define _SCREEN_H_
 
+#include "../screen_.h"
 #include "common.h"
 
 #define SCREEN_ADDRESS ((uint8_t*)0xb8000)
 #define SCREEN_WIDTH 80
 #define SCREEN_HEIGHT 25
-
-// enum Color {
-//     COLOR_BLACK         = 0x0,
-//     COLOR_BLUE          = 0x1,
-//     COLOR_GREEN         = 0x2,
-//     COLOR_AQUA          = 0x3,
-//     COLOR_RED           = 0x4,
-//     COLOR_PURPLE        = 0x5,
-//     COLOR_YELLOW        = 0x6,
-//     COLOR_WHITE         = 0x7,
-//     COLOR_GRAY          = 0x8,
-//     COLOR_LIGHT_BLUE    = 0x9,
-//     COLOR_LIGHT_GREEN   = 0xA,
-//     COLOR_LIGHT_AQUA    = 0xB,
-//     COLOR_LIGHT_RED     = 0xC,
-//     COLOR_LIGHT_PURPLE  = 0xD,
-//     COLOR_LIGHT_YELLOW  = 0xE,
-//     COLOR_BRIGHT_WHITE  = 0xF,
-// };
-
-// #define COLOR(text_color, background_color) ((((uint8_t)COLOR_##background_color) << 4) | ((uint8_t)COLOR_##text_color))
-
-// #define DEFAULT_COLOR COLOR(BRIGHT_WHITE, BLACK)
 
 void print_char(uint8_t x, uint8_t y, uint8_t character, uint8_t color);
 uint8_t print_string(uint8_t x, uint8_t y, char *string, uint8_t color);
--- shell/shell.c
+++ shell/shell.c
@@ -10,38 +10,6 @@
 #define PROMPT ">>"
 #define PROMPT_LENGTH (sizeof(PROMPT) - 1)
 
-// enum cmos_reg {
-//     CMOS_REG_SECONDS = 0x00,
-//     CMOS_REG_MINUTES = 0x02,
-//     CMOS_REG_HOURS = 0x04,
-//     CMOS_REG_DAY = 0x07,
-//     CMOS_REG_MONTH = 0x08,
-//     CMOS_REG_YEAR = 0x09,
-// };
-
-// enum Color {
-//     COLOR_BLACK         = 0x0,
-//     COLOR_BLUE          = 0x1,
-//     COLOR_GREEN         = 0x2,
-//     COLOR_AQUA          = 0x3,
-//     COLOR_RED           = 0x4,
-//     COLOR_PURPLE        = 0x5,
-//     COLOR_YELLOW        = 0x6,
-//     COLOR_WHITE         = 0x7,
-//     COLOR_GRAY          = 0x8,
-//     COLOR_LIGHT_BLUE    = 0x9,
-//     COLOR_LIGHT_GREEN   = 0xA,
-//     COLOR_LIGHT_AQUA    = 0xB,
-//     COLOR_LIGHT_RED     = 0xC,
-//     COLOR_LIGHT_PURPLE  = 0xD,
-//     COLOR_LIGHT_YELLOW  = 0xE,
-//     COLOR_BRIGHT_WHITE  = 0xF,
-// };
-
-// #define COLOR(text_color, background_color) ((((uint8_t)COLOR_##background_color) << 4) | ((uint8_t)COLOR_##text_color))
-
-// #define DEFAULT_COLOR COLOR(BRIGHT_WHITE, BLACK)
-
 uint8_t pos_y = 0;
 
 void*** arr_pointers = (void***)0x6C00;
